fbd8d2d150064da87ca0c99f6c58d804a752ab3f
Ben Visness <bvisness@gmail.com>
2018-10-01 22:05:09 -0500

    Hash stuff

--- build.js
+++ build.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
+const { execSync } = require('child_process');
 
-const raw = fs.readFileSync('vocab.csv', {
-    encoding: 'utf8',
-});
+const raw = fs.readFileSync('vocab.csv', { encoding: 'utf8' });
 
 const lines = raw.split('\r\n');
 const keys = lines[0].split(',');
@@ -27,5 +26,23 @@ if (!fs.existsSync('build')){
     fs.mkdirSync('build');
 }
 
-const output = `const vocab = ${JSON.stringify(words)}\n`;
-fs.writeFileSync('build/vocab.js', output);
+if (process.argv[2] === '--hash') {
+    const hash = execSync('git rev-parse --short HEAD', { encoding: 'utf8' }).trim();
+
+    const output = `const vocab = ${JSON.stringify(words)}\n`;
+    fs.writeFileSync(`build/vocab-${hash}.js`, output);
+
+    fs.renameSync('build/script.js', `build/script-${hash}.js`);
+    fs.renameSync('build/style.css', `build/style-${hash}.css`);
+    fs.renameSync('build/normalize.css', `build/normalize-${hash}.css`);
+
+    let hashed = fs.readFileSync('build/index.html', { encoding: 'utf8' });
+    hashed = hashed.replace(/vocab\.js/g, `vocab-${hash}.js`);
+    hashed = hashed.replace(/script\.js/g, `script-${hash}.js`);
+    hashed = hashed.replace(/style\.css/g, `style-${hash}.css`);
+    hashed = hashed.replace(/normalize\.css/g, `normalize-${hash}.css`);
+    fs.writeFileSync('build/index.html', hashed, 'utf8');
+} else {
+    const output = `const vocab = ${JSON.stringify(words)}\n`;
+    fs.writeFileSync(`build/vocab.js`, output);
+}
